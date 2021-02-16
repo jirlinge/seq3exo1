ce9ac379dbf3e8349e3580843dda4cf5b791c9da
jirlinge <julienirlinger@gmail.com>
2021-02-16 09:41:56 +0100

    deuxieme correction

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int a = 5;
-            int b = 5;
-            int c = a + b;
+            int Number1 = 5;
+            int Number2 = 5;
+            int Sum = a + b;
             Console.WriteLine("la premiere note est de "+a);
             Console.WriteLine("la premiere note est de " + b);
             Console.WriteLine("la somme des note est de " + c);
