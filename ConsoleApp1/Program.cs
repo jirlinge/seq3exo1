27873e51f040e45ba296a17638fbc83fe340ab90
jirlinge <julienirlinger@gmail.com>
2021-02-16 09:27:19 +0100

    premiere version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 4;
+            int c = a + b;
+            Console.WriteLine("la premiere note est de "+a);
+            Console.WriteLine("la premiere note est de " + b);
+            Console.WriteLine("la somme des note est de " + c);
+
+        }
+    }
+}
